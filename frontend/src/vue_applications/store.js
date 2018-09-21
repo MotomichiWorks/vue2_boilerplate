efc78e19cc48d3b3b7523e4efdb068a12fe3678d
motomichi <motomichi.motomichi@gmail.com>
2018-09-21 12:12:57 +0900

    change settings

---
+++ frontend/src/vue_applications/store.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const module1 = {
+  state: {},
+  mutations: {},
+  actions: {},
+  getters: {},
+}
+
+const state = {
+
+}
+
+const getters = {
+
+}
+
+const actions = {
+
+}
+
+const mutations = {
+
+}
+
+const store = new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations,
+  modules: {
+    module1,
+  }
+})
